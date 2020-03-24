a694383af6a0872d8b6e34f5f2274494ef270abd
Ivan Tolkachev <vantrey@yandex.ru>
2020-03-24 20:50:37 +0300

    start. complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import img from './dog.jpg'
+import voice from './dog.mp3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+
+  state = {
+    index: 0,
+    counter: 0
+  }
+
+  componentDidMount() {
+    setInterval(()=>{
+      let index = this.getRandomImageIndex()
+      this.setState({index})
+    }, 5000)
+  }
+
+  getRandomImageIndex = () => {
+    return Math.floor(Math.random() * 9);
+  }
+  player = new Audio(voice)
+
+  onImgClick = (e) => {
+    if (Number(e.currentTarget.id) === this.state.index) {
+      this.setState({counter: this.state.counter + 1})
+      this.player.play()
+    }
+  }
+
+  render = () => {
+    const dogs = Array.from({length: 9}).map((el, i) => {
+      let classForVisible
+      if (this.state.index === i) {
+        classForVisible =  'photo show'
+      } else {
+        classForVisible = 'photo'
+      }
+
+      return (
+        <div className="item"
+             onClick={this.onImgClick}
+             id={i}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img src={img} className={classForVisible}/>
+        </div>
+      )
+    })
+
+    return (
+      <div className='wrapper'>
+        {dogs}
+        <div className="counter">
+          {this.state.counter}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
